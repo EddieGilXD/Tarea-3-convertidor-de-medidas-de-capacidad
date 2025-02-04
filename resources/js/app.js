2be26fd697b98cd644a5dfe684b2f7e485b2dcea
EddieGilXD <djuntaied@gmail.com>
2025-02-04 15:56:08 -0600

    tarea 3 completamente terminada

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,29 @@ function convertir(){
     
     if (option == "L") {
         const resultadoLitros = cantidad;
-        const resultadoGalones = parseInt(cantidad / 3.785, 2);
-        console.log(resultadoLitros, resultadoGalones);
+        const resultadoGalones = cantidad / 3.79;
+        const resultadoBotellas = cantidad * 0.75
+        updateInfo(resultadoLitros, resultadoGalones, resultadoBotellas);
     } else if (option == "Gal"){
-        console.log("es galon")
+        const resultadoLitros = cantidad / 3.79;
+        const resultadoGalones = cantidad;
+        const resultadoBotellas = cantidad * 5.04;
+        updateInfo(resultadoLitros, resultadoGalones, resultadoBotellas);
     } else if (option == 'B') {
-        console.log("es botella")
+        const resultadoLitros = cantidad * 0.75;
+        const resultadoGalones = cantidad * 0.19;
+        const resultadoBotellas = cantidad;
+        updateInfo(resultadoLitros, resultadoGalones, resultadoBotellas);
     }
 
+    function updateInfo(resultadoLitros, resultadoGalones, resultadoBotellas) {
+        const litrosResult = document.getElementById("litrosResult");
+        const galonesResult = document.getElementById("galonesResult");
+        const botellasResult = document.getElementById("botellasResult");
+
+        litrosResult.textContent = resultadoLitros;
+        galonesResult.textContent = resultadoGalones;
+        botellasResult.textContent = resultadoBotellas;
+    }
+    
 }
